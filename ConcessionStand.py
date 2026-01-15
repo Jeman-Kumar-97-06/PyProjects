9ab34de04f659a4a31ce51e611664063f348d9b4
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-01-15 15:23:16 +0530

    made changes

---
+++ ConcessionStand.py
@@ -0,0 +1,17 @@
+menu = {
+    "pizze":3.00,
+    "nachos":4.50,
+    "popcorn":6.00,
+    "fries":2.50,
+    "chips":1.00,
+    "pretzel":3.50,
+    "soda":3.00,
+    "lemonade":4.25
+}
+
+cart = []
+total = 0
+
+for key,val in menu.items():
+    print(f"{key:10}:{val:.2f}")
+
