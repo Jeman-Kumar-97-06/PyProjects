a4832c0ed7525db2d12125c38fb8893d23e117fc
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-01-15 15:54:09 +0530

    Added ConcessionStand Program

--- ConcessionStand.py
+++ ConcessionStand.py
@@ -1,5 +1,5 @@
 menu = {
-    "pizze":3.00,
+    "pizza":3.00,
     "nachos":4.50,
     "popcorn":6.00,
     "fries":2.50,
@@ -12,6 +12,25 @@ menu = {
 cart = []
 total = 0
 
+print("------------MENU------------")
+
 for key,val in menu.items():
     print(f"{key:10}:{val:.2f}")
 
+print("----------------------------")
+
+while True:
+    food = input("Select an item (q to quit): ")
+    if food.lower() == 'q':
+        break
+    elif food in menu:
+        cart.append(food)
+
+print('---------------------------')
+for food in cart:
+    total = total + menu.get(food)
+    print(food,end=' ')
+
+print(f"Total is : ${total:.2f}")
+
+         
