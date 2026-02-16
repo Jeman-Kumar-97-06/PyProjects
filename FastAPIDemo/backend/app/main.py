9bb25d129c996d592d32d6e6675420990ac274b4
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-02-16 21:43:38 +0530

    made changes

--- FastAPIDemo/backend/app/main.py
+++ FastAPIDemo/backend/app/main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 #Import FastAPI from fastapi
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 #----------------------------------------------------------
 class ModelName(str, Enum):
@@ -9,6 +10,14 @@ class ModelName(str, Enum):
     lenet   = 'lenet'
 
 fake_items_db = [{"item_name":"Foo"},{"item_name":"Bar"},{"item_name":"Baz"}]
+
+
+class Item(BaseModel):
+    name:str
+    description:str | None = None #Optional
+    price:float
+    tax:float | None = None       #Optional
+
 #------------------------------------------------------------
 
 #Create a FastAPI instance
@@ -61,6 +70,47 @@ async def read_item(skip:int=0,limit:int=0):
     return fake_items_db[skip : skip+limit]
 #the above shit is dealt as : http://127.0.0.1:8000/items/?skip=0&limit=10
 
+@app.get('/items/{item_id}')
+async def read_item(item_id:str, q:str | None = None):
+    if q:
+        return {"item_id":item_id,"q":q}
+    return {"item_id":item_id}
+
+#multiple path and query parameters:
+@app.get('/users/{user_id}/items/{item_id}')
+async def read_user_item(user_id:int, item_id:str, q:str | None = None, short:bool = False):
+    item = {"item_id":item_id,"owner_id":user_id}
+    if q:
+        item.update({'q':q})
+    if not short:
+        item.update(
+            {"description":"This is an amazing item that has a long description"}
+        )
+    return item
+
+#Required Query Params : 
+@app.get('/items/{item_id}')
+async def read_user_item(item_id:str, needy:str):#here 'needy' is a required query parameter: It you don't include 'needy' you will see error.
+    item = {"item_id":item_id, "needy":needy}
+    return item
+
+#REQUEST BODY: DATA SENT BY CLIENT TO AN API:
+#Defining a Request body : ie., rules about what the fuck API accepts from a client:
+#Reads the 'item' as JSON.
+@app.post('/items/')
+async def create_item(item:Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax':price_with_tax})
+    return item_dict
+
+#-----------------------------------------------------------
+@app.post('/items/{item_id}')
+async def update_item(item_id:int, item:Item):
+    return {"item_id":item_id,**item.model_dump()}
 
 
 
+
+#RESPONSE BODY: DATA SENT BY AN API BACK TO A CLIENT:
