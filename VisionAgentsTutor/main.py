295523e025d4db6ce9076369745bf19aeb692b73
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-02-18 11:15:02 +0530

    made changes

---
+++ VisionAgentsTutor/main.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+
+from vision_agents.core import Agent, AgentLauncher, User, Runner
+from vision_agents.plugins import getstream, gemini
+
+load_dotenv()
+
+async def create_agent(**kwargs) -> Agent :
+    return Agent(
+        edge=getstream.Edge(),
+        agent_user=User(name='Assistant',id='agent'),
+        instructions='Describe what you see. Be cocise.',
+        llm=gemini.Realtime(fps=3), #Video frames/sec sent to model
+    )
+
+async def join_call(agent:Agent, call_type:str, call_id:str, **kwargs) -> None:
+    call = await agent.create_call(call_type,call_id)
+    async with agent.join(call):
+        await agent.simple_response("What do you see?")
+        await agent.finish()
+
+if __name__ == "__main__":
+    Runner(AgentLauncher(create_agent=create_agent, join_call=join_call)).cli()
+    
